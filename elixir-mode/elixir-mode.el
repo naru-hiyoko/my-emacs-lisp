e5ac48f60e4f6e5c50ba1eb65967393b42e010ca
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2019-03-29 18:34:56 +0900

    fix as defunp

--- elixir-mode/elixir-mode.el
+++ elixir-mode/elixir-mode.el
@@ -66,7 +66,7 @@
     ("Public Functions" "^\\s-*def[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
     ("Public Functions" "^\\s-*defun[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
     ("Private Functions" "^\\s-*defp[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
-    ("Private Functions" "^\\s-*defpt[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
+    ("Private Functions" "^\\s-*defunp[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
     ("Public Macros" "^\\s-*defmacro[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
     ("Private Macros" "^\\s-*defmacrop[ \n\t]+\\([a-z0-9_!\\?]+\\)\\(([^)]*)\\)*.*" 1)
     ("Delegates" "^\\s-*defdelegate[ \n\t]+\\([a-z0-9_]+\\)\\(([^)]*)\\)*.*" 1)
@@ -123,7 +123,7 @@
                           "with")
                       symbol-end))
       (builtin-declaration . ,(rx symbol-start
-                                  (or "def" "defun" "defp" "defpt" "defmodule" "defprotocol"
+                                  (or "def" "defun" "defp" "defunp" "defmodule" "defprotocol"
                                       "defmacro" "defmacrop" "defdelegate"
                                       "defexception" "defstruct" "defimpl"
                                       "defguard" "defguardp" "defcallback"
@@ -139,7 +139,7 @@
                          symbol-end))
       (function-declaration . ,(rx (or line-start (not (any ".")))
                                    symbol-start
-                                   (or "def" "defp" "defun" "defpt")
+                                   (or "def" "defp" "defun" "defunp")
                                    symbol-end))
       ;; The first character of an identifier must be a letter or an underscore.
       ;; After that, they may contain any alphanumeric character + underscore.
